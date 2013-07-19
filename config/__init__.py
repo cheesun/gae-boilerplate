39c728aa16fd6c702f1b02c142344375738bdc9e
Cheesun Choong <cchoong@fairfaxmedia.com.au>
2013-07-19 11:06:17 +1000

    create a base config for settings that are shared between all environments but specific to the app

--- config/__init__.py
+++ config/__init__.py
@@ -7,9 +7,12 @@ import os
 
 if "SERVER_SOFTWARE" in os.environ:
     if os.environ['SERVER_SOFTWARE'].startswith('Dev'):
-        from config.localhost import config
+        from config.localhost import config as environment_config
 
     elif os.environ['SERVER_SOFTWARE'].startswith('Google'):
-        from config.production import config
+        from config.production import config as environment_config
 else:
-    from config.testing import config
+    from config.testing import config as environment_config
+
+from config.base import config
+config.update(environment_config)
