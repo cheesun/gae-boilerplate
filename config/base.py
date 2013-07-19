c931f7e39f95b9257f177543a51d268a3e64c46e
Cheesun Choong <cchoong@fairfaxmedia.com.au>
2013-07-19 11:23:29 +1000

    add base config file

---
+++ config/base.py
@@ -0,0 +1,18 @@
+"""
+Config file for options that are shared between all environments.
+Settings here will take precedence over the config located in the boilerplate package,
+but be overridden by settings in the environment config files.
+"""
+config = {
+
+    # application name
+    'app_name': "Google App Engine Boilerplate",
+
+    # fellas' list
+    'developers': (
+        ('Santa Klauss', 'snowypal@northpole.com'),
+    ),
+
+    # ----> ADD MORE CONFIGURATION OPTIONS HERE <----
+
+}
